74693289e54651e3499a869d097ce93dde1b2ea3
David Bronfen <davidbronfen@gmail.com>
2017-07-20 17:41:41 +0300

    Added test to user list reducer

---
+++ src/app/reducers/users-list.spec.ts
@@ -0,0 +1,26 @@
+import { reducer } from './index';
+
+describe('reducers', () => {
+  it('Shoud handle users list action', () => {
+    let state;
+    state = reducer({
+      users: [{
+        name: '',
+        screen_name: '',
+        profile_image_url: ''
+      }]
+    },  {
+      type: '[UsersList] Load UsersList'
+    });
+    expect(state).toEqual({
+      users: [{
+        name: '',
+        screen_name: '',
+        profile_image_url: ''
+      }]
+    });
+  });
+
+  it('Should handle Load Users list Success action', () => { });
+
+})
