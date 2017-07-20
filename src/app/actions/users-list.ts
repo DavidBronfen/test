75811790767a44764e88f34cb19b15be73ad2f20
David Bronfen <davidbronfen@gmail.com>
2017-07-20 09:49:26 +0300

    Added users list action.

---
+++ src/app/actions/users-list.ts
@@ -0,0 +1,22 @@
+import { Action } from '@ngrx/store';
+import { IUser } from '../models/user';
+
+export const LOAD_USERS_LIST =  '[UsersList] Load UsersList';
+export const LOAD_USERS_LIST_SUCCESS = '[UsersList] Load UsersList Success';
+
+
+export class LoadUsersListAction implements Action {
+  readonly type = LOAD_USERS_LIST;
+
+  constructor() { }
+}
+
+export class LoadUsersListSuccessAction implements Action {
+  readonly type = LOAD_USERS_LIST_SUCCESS;
+
+  constructor(public payload: IUser[]) { }
+}
+
+export type Actions
+  = LoadUsersListAction
+  | LoadUsersListSuccessAction;
