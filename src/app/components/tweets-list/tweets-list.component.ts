f41d816de62adc4cb643aaa35ab2b531733563a9
David Bronfen <davidbronfen@gmail.com>
2017-07-20 20:41:33 +0300

    Remove commented code from tweets list component.

--- src/app/components/tweets-list/tweets-list.component.ts
+++ src/app/components/tweets-list/tweets-list.component.ts
@@ -27,24 +27,3 @@ export class TweetsListComponent implements OnInit {
   }
 
 }
-
-// [
-//   {
-//     'name': 'Nir Galon',
-//     'screen_name': 'nirgalon',
-//     'profile_image_url': 'https://api.adorable.io/avatars/285/nir.png',
-//     'text': 'I\'ve had these jeans since I was born And now they\'re ripped and now they\'re torn And all my friends have skateboards'
-//   },
-//   {
-//     'name': 'Nir Galon',
-//     'screen_name': 'nirgalon',
-//     'profile_image_url': '',
-//     'text': 'I want the toys of other boys I want a knife and a gun and things But Mum and Dad will not give in'
-//   },
-//   {
-//     'name': 'Nir Galon',
-//     'screen_name': 'nirgalon',
-//     'profile_image_url': 'https://api.adorable.io/avatars/285/nir.png',
-//     'text': 'And now I know just what it is It\'s got disease and it\'s got my head It always runs where I hide'
-//   },
-// ]
