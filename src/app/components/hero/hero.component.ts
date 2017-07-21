628c811d03826c842119d49cb6369fe69c7b02f1
David Bronfen <davidbronfen@gmail.com>
2017-07-21 19:04:16 +0300

    Hero component gets the responsibility to dispatch the user list action, taken from the users list.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,18 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { FormControl } from '@angular/forms';
+import { SlicePipe } from '@angular/common';
+
+import 'rxjs/add/operator/debounceTime';
+
+import { IUser } from '../../models/user';
+
+import * as usersListAction from '../../actions/users-list';
+import * as fromRoot from '../../reducers';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent {
+
+  @Output('updateUserListEvent')
+  updateUserListEvent = new EventEmitter<boolean>();
 
   title = 'Welcome to FED Test';
-  users = ['Nir', 'David', 'Roy'];
+  public users$: Observable<IUser[]>;
+  private errorMessage: string;
+  private term = new FormControl();
+  private showSearchResult = false;
 
-  constructor() { }
+  constructor(private store: Store<fromRoot.State>) {
+    this.users$ = this.store.select(fromRoot.getUsersListState);
 
-  ngOnInit() {
+    this.term.valueChanges
+     .debounceTime(500)
+     .subscribe(searchWord => {
+       if (searchWord) {
+         this.store.dispatch(new usersListAction.LoadUsersListAction(searchWord));
+         this.showSearchResult = true;
+       } else {
+         this.showSearchResult = false;
+       }
+     });
+  }
+
+  displayUsers(event) {
+    this.updateUserListEvent.emit(true);
+    this.showSearchResult = false;
   }
 
 }
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 
 import { IUser } from '../../models/user';
-import { UsersListService } from '../../services/users-list.service';
 
 import * as usersListAction from '../../actions/users-list';
 import * as fromRoot from '../../reducers';
@@ -13,17 +12,13 @@ import * as fromRoot from '../../reducers';
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersListComponent implements OnInit {
+export class UsersListComponent {
 
   public users$: Observable<IUser[]>;
   private errorMessage: string;
 
   constructor(private store: Store<fromRoot.State>) {
       this.users$ = this.store.select(fromRoot.getUsersListState);
-  }
-
-  ngOnInit() {
-    this.store.dispatch(new usersListAction.LoadUsersListAction())
   }
 
 }
