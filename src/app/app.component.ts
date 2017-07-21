3f713b2f25c7c7c4a4e54a16fbd384c8dd459c65
David Bronfen <davidbronfen@gmail.com>
2017-07-21 19:23:02 +0300

    App component toggle the search result.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private showUserList = false;
+  private showTweetList = false;
+
+  updateUserList(res) {
+    console.log('res', res);
+    this.showUserList = res;
+  }
 }
