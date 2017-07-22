70935cb3805f35c57c9b4233090a1e0d52fcc0ab
David Bronfen <davidbronfen@gmail.com>
2017-07-22 10:26:53 +0300

    Changed function name.

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -24,7 +24,7 @@ export class UsersListComponent {
       this.users$ = this.store.select(fromRoot.getUsersListState);
   }
 
-  displayUserTweet(id) {
+  displayUserTweets(id) {
     this.updateTweetListEvent.emit(true);
     this.store.dispatch(new tweetsListAction.LoadTweetsListAction(id))
   }
