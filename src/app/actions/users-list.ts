e967077af6b6f9abfee2a0ae3e4e6ea53388784a
David Bronfen <davidbronfen@gmail.com>
2017-07-22 12:10:58 +0300

    New action is added - Update Selected User.

--- src/app/actions/users-list.ts
+++ src/app/actions/users-list.ts
@@ -3,6 +3,7 @@ import { IUser } from '../models/user';
 
 export const LOAD_USERS_LIST =  '[UsersList] Load UsersList';
 export const LOAD_USERS_LIST_SUCCESS = '[UsersList] Load UsersList Success';
+export const UPDATE_SELECTED_UUSER = '[UsersList] Update Selected User';
 
 
 export class LoadUsersListAction implements Action {
@@ -17,6 +18,13 @@ export class LoadUsersListSuccessAction implements Action {
   constructor(public payload: IUser[]) { }
 }
 
+export class UpdateSelectedUserAction implements Action {
+  readonly type = UPDATE_SELECTED_UUSER;
+
+  constructor(public payload: String) { }
+}
+
 export type Actions
   = LoadUsersListAction
-  | LoadUsersListSuccessAction;
+  | LoadUsersListSuccessAction
+  | UpdateSelectedUserAction;
